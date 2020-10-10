7bb1462cae4f6333eb2cfe1d946d026e939574e8
panjunqing <panjunqing@ke.com>
2020-10-10 15:23:42 +0800

    temp

--- examples/sql.js
+++ examples/sql.js
@@ -2,33 +2,9 @@
 
 // Run me with Node to see my output!
 
-let util = require("util");
 let P = require("..");
 
 ///////////////////////////////////////////////////////////////////////
-
-// Turn escaped characters into real ones (e.g. "\\n" becomes "\n").
-function interpretEscapes(str) {
-    let escapes = {
-        b: "\b",
-        f: "\f",
-        n: "\n",
-        r: "\r",
-        t: "\t"
-    };
-
-    return str.replace(/\\(u[0-9a-fA-F]{4}|[^u])/, (_, escape) => {
-        let type = escape.charAt(0);
-        let hex = escape.slice(1);
-        if (type === "u") {
-            return String.fromCharCode(parseInt(hex, 16));
-        }
-        if (escapes.hasOwnProperty(type)) {
-            return escapes[type];
-        }
-        return type;
-    });
-}
 
 // Use the JSON standard's definition of whitespace rather than Parsimmon's.
 let whitespace = P.regexp(/\s*/m);
@@ -45,20 +21,106 @@ function word(str) {
 }
 
 function fieldType(type) {
-    return P.seqMap(P.string(type),P.alt(token(P.string("(").then(P.digits).skip(P.string(")"))),P.optWhitespace),function(t,l) {
+    return P.seqMap(
+        P.string(type),
+        P.alt(token(P.string("(").then(P.digits).skip(P.string(")"))),P.optWhitespace),
+        P.alt(token(P.string("(").then(P.digits).skip(word(",")).then(P.digit).skip(P.string(")"))),P.optWhitespace),
+        function(t,l,l2) {
         return t;
     }) 
+}
+
+function allTypes() {
+    return P.alt(
+        // numeric
+        fieldType("tinyint"),
+        fieldType("smallint"),
+        fieldType("mediumint"),
+        fieldType("int"),
+        fieldType("bigint"),
+        fieldType("float"),
+        fieldType("double"),
+        fieldType("decimal"),
+        // char
+        fieldType("varchar"),
+        fieldType("char"),
+        fieldType("tinytext"),
+        fieldType("text"),
+        fieldType("mediumtext"),
+        fieldType("longtext"),
+        //time
+        fieldType("datetime"),
+        fieldType("date"),
+        fieldType("time"),
+        fieldType("timestamp"),
+        //blob
+        fieldType("tinyblob"),
+        fieldType("blob"),
+        fieldType("mediumblob"),
+        fieldType("longblob"),
+        //binary
+        fieldType("binary"),
+        fieldType("varbinary")
+    );
+}
+
+function allAttributes() {
+    return P.alt(word("not null"),
+                 word("null"),
+                 word("auto_increment"),
+                 word("primary key"),
+                 word("unique"),
+                 word("binary"),
+                 word("unsigned"));
+}
+
+function orEmpty(name) {
+    return P.alt(token(word(name)),P.optWhitespace);
+}
+
+function argumentValue(name) {
+    return P.alt(
+        word(name).skip(P.string("'")).then(P.takeWhile( function(x) { return x !== "'"; })).skip(P.string("'")).skip(whitespace),
+        P.seqMap(word(name),orEmpty("current_timestamp"),orEmpty("on"),orEmpty("update"),orEmpty("current_timestamp"),
+        function(a,b,c,d,e) {
+            if(e) {
+                return [b,c,d,e].join(" ");
+            }else {
+                return b;
+            }
+        }),
+        P.optWhitespace);
+}
+
+function skipToRBracket() {
+    return P.takeWhile(function(x) {
+        return x !== ")";
+    });
+}
+
+function skipTo(text) {
+        return P.custom(function(success, failure) {
+          return function(input, i) {
+            var index = input.lastIndexOf("comment");
+            var lastBracket = input.lastIndexOf(")");
+            if(index > 0 && index > lastBracket) {
+              var result = P.all.wrap(word("'"),word("'")).tryParse(input.substr(index));
+              return success(index, result);
+            }
+            return failure(i, "skip to " + text + "failure");
+          };
+        });
 }
 
 let JSONParser = P.createLanguage({
     // This is the main entry point of the parser: a full JSON value.
     value: r =>
-        P.seqMap(r.create,r.name,r.lbracket,r.fields,P.all,function(create,tableName,ignore1,fields,ignore2) {
+        P.seqMap(r.create,r.name,r.lbracket,r.fields, skipTo("comment"),function(create,tableName,ignore1,fields,comment) {
             return {tableName,fields}
         }),
      // r.create.then(r.name).skip(r.lbracket).then(r.fields).skip(P.all),
     // The basic tokens in JSON, with optional whitespace afterward.
-    create: () => word("CREATE TABLE"),
+    create: () => token(word("create table")),
     lbracket: () => word("("),
     rbracket: () => word(")"),
     quote: () => word("`"),
@@ -67,22 +129,19 @@ let JSONParser = P.createLanguage({
 
     // Regexp based parsers should generally be named for better error reporting.
     name: r =>
-        P.alt(token(P.regexp(/[a-z|A-Z|_]+/)),token(P.regexp(/`[a-z|A-Z|_]+`/)))
-            .map(interpretEscapes)
-            .desc("name"),
+        token(P.regexp(/[a-z|A-Z|_]+/)
+        .wrap(P.alt(P.string("`"),P.optWhitespace),P.alt(P.string("`"),P.optWhitespace)))
+        .desc("name"),
     fields: r =>
-        P.seqMap(r.name,r.type,P.letters,function(name,type,others) {
-            return {name,type}
+        P.seqMap(r.name,r.type,r.attributes.trim(P.optWhitespace).many(),
+        argumentValue("default"),
+        argumentValue("comment"),
+        function(name,type,others,defaults,comment) {
+            return {name,type,defaults,comment}
         }).sepBy(r.comma)
         .desc("fields"),
-    type: () =>
-        P.alt(fieldType("int"),
-              fieldType("varchar")),
-
-    number: () =>
-        token(P.regexp(/-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][+-]?[0-9]+)?/))
-            .map(Number)
-            .desc("number")
+    type: () => allTypes(),
+    attributes: () => allAttributes(),
 });
 
 ///////////////////////////////////////////////////////////////////////
@@ -91,8 +150,8 @@ let JSONParser = P.createLanguage({
 let text = `\
 CREATE TABLE \`user\` (
     user_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_name varchar(255) binary NOT NULL default '',
-    user_real_name varchar(255) binary NOT NULL default '',
+    \`user_name\` varchar(255) binary NOT NULL default 'xxxx',
+    user_real_name varchar(255) binary NOT NULL default 'abc',
     user_password tinyblob NOT NULL,
     user_newpassword tinyblob NOT NULL,
     user_newpass_time binary(14),
@@ -109,20 +168,32 @@ CREATE TABLE \`user\` (
 `;
 */
 
+let text = `CREATE TABLE \`t_org_back\` (
+    \`id\` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增编号',
+    \`orgCode\` varchar(50) NOT NULL DEFAULT '' COMMENT '组织编码',
+    \`orgType\` int(11) NOT NULL DEFAULT '0' COMMENT '组织类型',
+    \`name\` varchar(50) NOT NULL DEFAULT '' COMMENT '组织简称',
+    \`parentOrgCode\` varchar(50) NOT NULL DEFAULT '-1' COMMENT '父组织编码',
+    \`fullName\` varchar(200) NOT NULL DEFAULT '' COMMENT '组织全称',
+    \`note\` varchar(500) NOT NULL DEFAULT '' COMMENT '组织全称',
+    \`treeLevel\` int(11) NOT NULL DEFAULT '0' COMMENT '组织深度',
+    \`status\` int(11) NOT NULL DEFAULT '1' COMMENT '组织状态，1有效，0失效',
+    \`orgLongCode\` varchar(300) NOT NULL DEFAULT '' COMMENT '组织树长编码',
+    \`ctime\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+    \`mtime\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    \`cuser\` bigint(20) NOT NULL DEFAULT '0' COMMENT '数据创建人',
+    \`muser\` bigint(20) NOT NULL DEFAULT '0' COMMENT '数据修改人',
+    PRIMARY KEY (\`id\`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='组织结构备份表';
+`
 
-let text = `\
-CREATE TABLE \`user\` (
-    user_id int(22),
-    user_name varchar
-   );
-`;
+// let text = `\
+// CREATE TABLE \`user\` (
+//     user_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+//     user_name varchar(255) binary,
+//     usexr_name varchar(255) binary NOT NULL default 'xxxx'
+//    );
+// `;
 
-function prettyPrint(x) {
-    let opts = { depth: null, colors: "auto" };
-    let s = util.inspect(x, opts);
-    console.log(s);
-}
-
-let ast = JSONParser.value.tryParse(text);
+let ast = JSONParser.value.tryParse(text.toLowerCase());
 console.log(ast);
-prettyPrint(ast);
